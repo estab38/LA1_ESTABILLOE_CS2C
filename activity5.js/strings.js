dabba3e66741c89cf5162d31716652dee9c32363
Estabillo Ermalyn <ermalyncalindas19@gmail.com>
2024-09-23 08:33:58 +0800

    Create strings.js

---
+++ activity5.js/strings.js
@@ -0,0 +1,7 @@
+let favActorFirstName = "Kathryn";
+let favActorLastName = " Bernardo";
+let fullName = favActorFirstName + favActorLastName;          
+                    
+let message = "My favorite actor is " + fullName;
+message += " his best show is got to believe"
+console.log(message.toUpperCase());
