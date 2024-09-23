31dc303bf7e7b7fa11360855d664530e7bf71aa5
Estabillo Ermalyn <ermalyncalindas19@gmail.com>
2024-09-23 08:32:34 +0800

    Create comparison.js

---
+++ activity4.js/comparison.js
@@ -0,0 +1,11 @@
+let firstFavNumb = 15;
+let seconFavNumb = 13;
+
+console.log(firstFavNumb > seconFavNumb)
+console.log(firstFavNumb < seconFavNumb)
+console.log(firstFavNumb >= seconFavNumb)
+console.log(firstFavNumb <= seconFavNumb)
+console.log(firstFavNumb === seconFavNumb)
+console.log(firstFavNumb == seconFavNumb)
+console.log(firstFavNumb !== seconFavNumb)
+console.log(firstFavNumb != seconFavNumb)
