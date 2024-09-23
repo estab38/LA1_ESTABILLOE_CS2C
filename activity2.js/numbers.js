a36f6d658652fdd86542b235133fa7cdf4a10a59
Estabillo Ermalyn <ermalyncalindas19@gmail.com>
2024-09-23 08:25:49 +0800

    Create numbers.js

---
+++ activity2.js/numbers.js
@@ -0,0 +1,9 @@
+let firstFavNumb = 15;
+let secondFavNumb = 13;
+
+console.log(firstFavNumb + secondFavNumb)
+console.log(firstFavNumb - secondFavNumb)
+console.log(firstFavNumb * secondFavNumb)
+console.log(firstFavNumb / secondFavNumb)
+console.log(firstFavNumb % secondFavNumb)
+console.log(firstFavNumb ^ secondFavNumb)
