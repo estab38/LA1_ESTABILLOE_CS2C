52d8478e1454c3210ebeae5028d1f174331b511e
Estabillo Ermalyn <ermalyncalindas19@gmail.com>
2024-09-23 08:22:43 +0800

    Create variable.js

---
+++ activity1./variable.js
@@ -0,0 +1,9 @@
+let Name ="Ermalyn Estabillo";
+let whatDoYouWannaBeInYourLife = "Programmer";
+let gender = "female";
+let tiktokHandle = "smaiii"
+
+console.log(Name)
+console.log(whatDoYouWannaBeInYourLife)
+console.log(gender)
+console.log(tiktokHndle)
